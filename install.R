e03c7d561b428a427bc6cb7d280905d32b98d2a9
Mara Tate <89354755+maratate@users.noreply.github.com>
2025-10-23 14:37:28 -0400

    Update install.R

--- install.R
+++ install.R
@@ -1,5 +1,8 @@
 install.packages("tidyverse", dependencies = TRUE)
 install.packages("rmarkdown", dependencies = TRUE)
 install.packages("IRkernel", dependencies = TRUE)
+install.packages("shiny"), dependencies = TRUE)
+install.packages("dplyr"), dependencies = TRUE)
+
 
 IRkernel::installspec(user = FALSE)
