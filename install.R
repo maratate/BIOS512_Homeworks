af58b8eb29d3aa4518c95ec985810574688adb72
Mara Tate <89354755+maratate@users.noreply.github.com>
2025-11-05 15:24:07 -0500

    Update install.R

--- install.R
+++ install.R
@@ -3,6 +3,6 @@ install.packages("rmarkdown", dependencies = TRUE)
 install.packages("IRkernel", dependencies = TRUE)
 install.packages("shiny"), dependencies = TRUE)
 install.packages("dplyr"), dependencies = TRUE)
-
+install.packages("readr"), dependencies = TRUE)
 
 IRkernel::installspec(user = FALSE)
